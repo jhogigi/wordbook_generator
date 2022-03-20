c42ee4521d6378124cf9ed873ee31ee18167510d
koki <s17161278@st.daito.ac.jp>
2022-03-20 11:58:58 +0000

    Translatorでキャッシュを使うように修正

--- tasks/translator.py
+++ tasks/translator.py
@@ -19,10 +19,14 @@ class Translator:
 
     @staticmethod
     def _translate(wordname: str) -> str:
-        scrape_url = 'https://ejje.weblio.jp/content/'
-        res = requests.get(scrape_url + wordname)
-        soup = BeautifulSoup(res.text, 'html.parser')
-        if meaning_html := soup.find(class_='content-explanation'):
-            return meaning_html.get_text()
-
+        cached_morph = Morph.objects.filter(wordname=wordname)
+        if cached_morph:
+            return cached_morph[0].meaning
+        else:
+            scrape_url = 'https://ejje.weblio.jp/content/'
+            res = requests.get(scrape_url + wordname)
+            soup = BeautifulSoup(res.text, 'html.parser')
+            if meaning_html := soup.find(class_='content-explanation'):
+                return meaning_html.get_text()
+        return None
         
