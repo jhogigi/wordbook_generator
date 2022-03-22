8d054a1de6369c4c01ebb21752e77c3503af33f3
koki <s17161278@st.daito.ac.jp>
2022-03-22 11:12:43 +0000

    アップロードページを/file_upload/ -> /にお引越し

--- file_manager/urls.py
+++ file_manager/urls.py
@@ -5,7 +5,7 @@ from file_manager.views import WaitingTaskPage, ResultTaskPage
 
 
 urlpatterns = [
-    path('file_upload/', FileManagerView.as_view()),
+    path('', FileManagerView.as_view()),
     path('waiting_task/<uuid:task_id>/', WaitingTaskPage.as_view()),
     path('task_result/<uuid:task_id>/', ResultTaskPage.as_view()),
 ]
