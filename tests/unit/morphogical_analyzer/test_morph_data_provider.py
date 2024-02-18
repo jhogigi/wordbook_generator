1fe670d5c0b97c772adc541f3b78b1d79f76dcae
jhogigi <jhogigifvuiyucgigi@gmail.com>
2024-02-18 13:44:33 +0900

    test: morph_data_providerのモジュールのテストに品詞情報を付与する振る舞いのテストを追加

--- tests/unit/morphogical_analyzer/test_morph_data_provider.py
+++ tests/unit/morphogical_analyzer/test_morph_data_provider.py
@@ -4,23 +4,25 @@ from morphogical_analyzer.morph_data_provider import MorphDataProvider
 
 
 class TestMorphDataProvider:
-    def test_単語walkingをwalkに変換する(self):
+    def test_単語walkingをwalkに変換して品詞情報を付与する(self):
         sentences = [
             'Walking down the street'
         ]
-        actual = MorphDataProvider.generate_normalized_data(sentences)
-        assert actual[0][0] == "walk"
+        result = MorphDataProvider.generate_normalized_data(sentences)
+        assert result[0][0] == "walk"
+        assert result[0][1] == "VERB"
 
-    def test_単語foundをfindに変換する(self):
+    def test_単語foundをfindに変換して品詞情報を付与する(self):
         sentences = [
             'I found a 1000-yen bill'
         ]
-        actual = MorphDataProvider.generate_normalized_data(sentences)
-        assert actual[0][0] == "find"
+        result = MorphDataProvider.generate_normalized_data(sentences)
+        assert result[0][0] == "find"
+        assert result[0][1] == "VERB"
 
     def test_ストップワードdown_the_I_を除去する(self):
         sentences = [
             'Walking down the street, I found a 1000-yen bill.'
         ]
-        actual = MorphDataProvider.generate_normalized_data(sentences)
-        assert len(actual) == 5
+        result = MorphDataProvider.generate_normalized_data(sentences)
+        assert len(result) == 5
